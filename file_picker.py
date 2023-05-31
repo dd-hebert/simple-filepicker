8079aef9dc2a96cdbce99b5bf812ae70eb117a28
David Hebert <39543390+dd-hebert@users.noreply.github.com>
2023-05-31 00:50:50 -0400

    Update file_picker.py

--- file_picker.py
+++ file_picker.py
@@ -8,7 +8,6 @@ Created on Fri May 27 2023
 """
 
 import os
-import pdb
 
 class FilePicker:
     """
@@ -26,7 +25,7 @@ class FilePicker:
 
     def __init__(self, root, file_ext):
         """
-        Create a FilePicker object.
+        Initialize a FilePicker object.
 
         Parameters
         ----------
@@ -41,33 +40,18 @@ class FilePicker:
 
         """
         self.file_list = []
-        self.root = root
+        self.root = os.path.abspath(root)
         self.ext = file_ext
 
-        # Get names of subfolders containing files
-        # in the :attr:`~FilePicker.root` directory.
-        for path, subdirs, _ in os.walk(self.root):
-            for name in subdirs:
-                rel_path = os.path.join(os.path.relpath(path, root), name)
-                # Each folder gets stored as a tuple.
-                # The 1st value is the folder name.
-                # The 2nd value is a list of files.
-                self.file_list.append((rel_path, []))
-                for file in os.listdir(os.path.join(path, name)):
-                    if file.endswith(self.ext):
-                        self.file_list[-1][1].append(file)
-                # Pop if folder contains no files
-                if self.file_list[-1][1] == []:
-                    self.file_list.pop(-1)
+        self.file_list = [
+            (
+                os.path.relpath(path, self.root),
+                [file for file in files if os.path.splitext(file)[1].lower() == self.ext.lower()]
+                )
+            for path, subdirs, files in os.walk(self.root)
+            ]
 
-        # Handle files directly in the root directory (no subfolder).
-        self.file_list.append((self.root, []))
-        for file in os.listdir(self.root):
-            if file.endswith(self.ext):
-                self.file_list[-1][1].append(file)
-        # Pop if no files direcly in the root directory
-        if self.file_list[-1][1] == []:
-            self.file_list.pop(-1)
+        self.file_list = [(folder, files) for folder, files in self.file_list if files]
 
     def pick_file(self):
         """
@@ -75,11 +59,10 @@ class FilePicker:
 
         Returns
         -------
-        string
+        file_path : string
             Returns the path of the chosen file relative to the root directory.
 
         """
-
         def print_folders_in_root():
             # Print list of folders in root directory
             print(f'\n{self.root}')
@@ -128,7 +111,7 @@ class FilePicker:
             if user_file in ['q', 'Q']:
                 return None
             elif user_file in ['b', 'B']:
-                self.pick_file()
+                return 'back'  # Indicates going back
             elif user_file.isnumeric() is False or int(user_file) not in accepted_range:
                 print_files_in_folder(folder_index, folder_name)
                 print('\nInvalid selection. Input a file number (shown in brackets)',
@@ -153,18 +136,20 @@ class FilePicker:
 
             return file_path
 
-        def file_picker_loop():
+        while True:
             print_folders_in_root()
             folder_choice = get_folder_choice()
-            if folder_choice is not None:
-                folder_index, folder_name = folder_choice
-                print_files_in_folder(folder_index, folder_name)
-                file_choice = get_file_choice(folder_index, folder_name)
-                if file_choice is not None:
-                    return print_selection(folder_name, file_choice)
-            return self.pick_file()
-
-        return file_picker_loop()
+            if folder_choice is None:
+                return None  # User wants to quit
+            folder_index, folder_name = folder_choice
+            print_files_in_folder(folder_index, folder_name)
+            file_choice = get_file_choice(folder_index, folder_name)
+            if file_choice is None:
+                return None  # User wants to quit
+            elif file_choice == 'back':
+                continue  # Go back to folder selection
+            else:
+                return print_selection(folder_name, file_choice)
 
     def tree(self):
         """
@@ -193,6 +178,6 @@ class FilePicker:
                         print(f'    └───{file}\t')
 
 if __name__ == '__main__':
-    fp = FilePicker(r'H:\Documents\Lab Notebook', '.KD')
+    fp = FilePicker(r'H:\Documents\Lab Notebook', 'KD')
 
-    output = fp.pick_file2()
+    file_list = fp.tree()
