99a90dbe74347e8140d5aa67832278c4a22d87fd
David Hebert <39543390+dd-hebert@users.noreply.github.com>
2023-06-14 19:15:44 -0400

    prepare for packaging

---
+++ simplefilepicker/__init__.py
@@ -0,0 +1,11 @@
+"""
+simplefilepicker
+================
+Simple utilities to view directories and pick files from the terminal.
+
+Created on Wed Jun 14 2023
+
+@author: David Hebert
+"""
+__author__ = 'David Hebert'
+__version__ = '0.0.1'
