fa21047b5a726d98857e098f8950bfedb49d5056
David Hebert <39543390+dd-hebert@users.noreply.github.com>
2024-03-16 14:58:17 -0400

    Update filepicker.py
    doc string change

--- simplefilepicker/filepicker.py
+++ simplefilepicker/filepicker.py
@@ -50,7 +50,9 @@ class FilePicker:
 
         Returns
         -------
-        None.
+        file_list : list
+            A list of files.
+
         """
         print(f'Searching "{self.root}" for {self.ext} files...')
 
